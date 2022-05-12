031e31db19d56b214c8e4efb029b31bb70be4669
tomb00105 <88777684+tomb00105@users.noreply.github.com>
2022-05-12 16:44:18 +0100

    Level Generation
    - Added random lillypad and player placement at start of game.
    - Added tongue animation.
    - Added collision detection for sides of level.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    GameObject player;
+
+    public GameObject lilypadPrefab;
+
+    public Transform lastLilypad;
+
+    public List<GameObject> lilypads = new List<GameObject>();
+
+    public int difficulty = 1;
+
+    [Range(0, 5)]
+    public int lives = 2;
+
+    public bool isAlive = true;
+
+    public int HP = 5;
+
+    public float totalTime = 0;
+
+    public int score = 0;
+
+    private void Start()
+    {
+        SpawnLevel();
+    }
+
+    //Handles logic if the player finishes their jump on water.
+    public void HitWater()
+    {
+        lives -= 1;
+        if (lives <= 0)
+        {
+            isAlive = false;
+            lives = 0;
+            Destroy(player);
+            //Splash effects
+            GameOver();
+        }
+        else
+        {
+            //Splash effects
+            player.transform.position = lastLilypad.position;
+        }
+    }
+
+    //Handles logic if player hits a fence.
+    public void HitFence()
+    {
+        lives -= 1;
+        if (lives <= 0)
+        {
+            isAlive = false;
+            lives = 0;
+            Destroy(player);
+            //Fence hit effects
+            GameOver();
+        }
+        else
+        {
+            //Fence hit effects
+            player.transform.position = lastLilypad.position;
+        }
+    }
+
+    //Spawns lilypads at random locations and then randomly chooses a starting lilypad for the player.
+    void SpawnLevel()
+    {
+        //Spawns fewer lilypads the higher the difficulty.
+        for (int i = 0; i < 12 - difficulty; i++)
+        {
+            bool locationFound = false;
+            //Takes random locations within bounds and checks that it isn't too close to an existing lilypad.
+            while (!locationFound)
+            {
+                Vector2 possibleLocation = new Vector2(Random.Range(-4.5f, 4.5f), Random.Range(-4.5f, 4.5f));
+
+                //Instantiates first lilypad.
+                if (lilypads.Count == 0)
+                {
+                    GameObject newLilypad = Instantiate(lilypadPrefab, possibleLocation, Quaternion.identity);
+                    newLilypad.transform.parent = GameObject.Find("Lilypads").transform;
+                    lilypads.Add(newLilypad);
+                    locationFound = true;
+                }
+                else
+                {
+                    for (int j = 0; j < lilypads.Count; j++)
+                    {
+                        //If too close to an existing lilypad, breaks and tries another random position.
+                        if (Vector2.Distance(lilypads[j].transform.position, possibleLocation) <= 1.4f)
+                        {
+                            break;
+                        }
+                        //If the loop reaches the last lilypad and it isn't too close, instantiates a lilypad at the location and adds it to the list of lilypads.
+                        else if (lilypads[j] == lilypads[lilypads.Count - 1])
+                        {
+                            GameObject newLilypad = Instantiate(lilypadPrefab, possibleLocation, Quaternion.identity);
+                            newLilypad.transform.parent = GameObject.Find("Lilypads").transform;
+                            lilypads.Add(newLilypad);
+                            locationFound = true;
+                        }
+                    }
+                }
+            }
+        }
+        //Chooses a random starting lilypad and instantiates the player there.
+        GameObject startLilypad = lilypads[Random.Range(0, lilypads.Count)];
+        Instantiate(playerPrefab, startLilypad.transform.position, Quaternion.identity);
+        player = GameObject.Find("Player(Clone)");
+    }
+
+
+    //Triggered when player lives reaches 0.
+    void GameOver()
+    {
+        Time.timeScale = 0;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    GameManager gameManager;
+
+    [SerializeField]
+    Animator tongueAnimator;
+
+    [SerializeField]
+    BoxCollider2D tongueCollider;
+
     [SerializeField, Range(0f, 10f)]
     float jumpPower = 0f;
 
@@ -13,86 +22,110 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 jumpToPoint;
 
+    [SerializeField]
     private Transform targetJump;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        gameManager.lastLilypad = CheckForLilypad(transform.position);
+    }
 
     private void Update()
     {
-        //Gets direction to mouse cursor from the player.
-        Vector2 dirToMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        dirToMouse.Normalize();
-
-        //Moves the player towards the calculated target position and handles logic for if they land on a lilypad or not.
-        if (isJumping)
+        if (gameManager.isAlive)
         {
-            transform.position = Vector2.Lerp(transform.position, jumpToPoint, jumpMultiplier * Time.deltaTime);
-            if (Vector2.Distance(transform.position, jumpToPoint) < 0.1f)
+            //Turns on the tongue collider if it is animating.
+            tongueCollider.enabled = tongueAnimator.GetCurrentAnimatorStateInfo(0).IsName("Tongue");
+
+            //Gets direction to mouse cursor from the player.
+            Vector2 dirToMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            dirToMouse.Normalize();
+
+            //Triggers the tongue animation when the left mouse button is clicked.
+            if (Input.GetMouseButtonDown(0))
             {
-                if (targetJump != null)
-                {
-                    isJumping = false;
-                    targetJump = null;
-                }
-                else
-                {
-                    //DEAD
-                }
+                tongueAnimator.SetTrigger("Active");
+                tongueCollider.enabled = true;
             }
-        }
 
-        //Handles logic for when the player is not currently jumping.
-        if (!isJumping)
-        {
-            //Sets player rotation towards the mouse cursor.
-            float rot_z = Mathf.Atan2(dirToMouse.y, dirToMouse.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
-
-            //While the player holds down the space key, the jump distance increases.
-            if (Input.GetKey(KeyCode.Space))
+            //Moves the player towards the calculated target position and handles logic for if they land on a lilypad or not.
+            if (isJumping)
             {
-                jumpPower += jumpMultiplier * Time.deltaTime;
-                if (jumpPower >= 10f)
+                transform.position = Vector2.Lerp(transform.position, jumpToPoint, jumpMultiplier * Time.deltaTime);
+                if (Vector2.Distance(transform.position, jumpToPoint) < 0.1f)
                 {
-                    jumpPower = 10f;
-                }
-            }
-            //If the player right clicks, jump power is set back to 0 so they can cancel a jump if they wish.
-            if (Input.GetKey(KeyCode.Mouse1))
-            {
-                jumpPower = 0;
-            }
-            //Handles logic for when the space key is released.
-            if (Input.GetKeyUp(KeyCode.Space))
-            {
-                if (jumpPower > 0)
-                {
-                    //Sets isJumping to true to ensure the player can't control the frog while in flight.
-                    isJumping = true;
-                    //Calculates the distance the player will jump and what position this ends at.
-                    Vector2 jumpDistance = dirToMouse * jumpPower;
-                    jumpToPoint = new Vector2(transform.position.x + jumpDistance.x, transform.position.y + jumpDistance.y);
-
-                    /*
-                    Debugging for jump distances
-                    Debug.Log("Direction to mouse: " + dirToMouse.ToString());
-                    Debug.Log("Jump Power: " + jumpPower.ToString());
-                    Debug.Log("Jump to point: " + jumpToPoint.ToString());
-                    */
-
-                    //Resets jump power for next jump.
-                    jumpPower = 0f;
-
-                    //Sets the target lilypad for the jump if the player is going to land on one.
-                    if (targetJump == null)
+                    if (targetJump != null)
                     {
-                        targetJump = CheckSafeJump(jumpToPoint);
+                        gameManager.lastLilypad = targetJump;
+                        isJumping = false;
+                        targetJump = null;
+                    }
+                    else
+                    {
+                        targetJump = null;
+                        isJumping = false;
+                        gameManager.HitWater();
+                    }
+                }
+            }
+
+            //Handles logic for when the player is not currently jumping.
+            if (!isJumping)
+            {
+                //Sets player rotation towards the mouse cursor.
+                float rot_z = Mathf.Atan2(dirToMouse.y, dirToMouse.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+                //While the player holds down the space key, the jump distance increases.
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    jumpPower += jumpMultiplier * Time.deltaTime;
+                    if (jumpPower >= 10f)
+                    {
+                        jumpPower = 10f;
+                    }
+                }
+                //If the player right clicks, jump power is set back to 0 so they can cancel a jump if they wish.
+                if (Input.GetKey(KeyCode.Mouse1))
+                {
+                    jumpPower = 0;
+                }
+                //Handles logic for when the space key is released.
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    if (jumpPower > 0)
+                    {
+                        //Sets isJumping to true to ensure the player can't control the frog while in flight.
+                        isJumping = true;
+                        //Calculates the distance the player will jump and what position this ends at.
+                        Vector2 jumpDistance = dirToMouse * jumpPower;
+                        jumpToPoint = new Vector2(transform.position.x + jumpDistance.x, transform.position.y + jumpDistance.y);
+
+                        /*
+                        Debugging for jump distances
+                        Debug.Log("Direction to mouse: " + dirToMouse.ToString());
+                        Debug.Log("Jump Power: " + jumpPower.ToString());
+                        Debug.Log("Jump to point: " + jumpToPoint.ToString());
+                        */
+
+                        //Resets jump power for next jump.
+                        jumpPower = 0f;
+
+                        //Sets the target lilypad for the jump if the player is going to land on one.
+                        if (targetJump == null)
+                        {
+                            targetJump = CheckForLilypad(jumpToPoint);
+                        }
                     }
                 }
             }
         }
+        
     }
 
-    //Checks whether the current jump will land on a lilypad and, if it will, returns it's transform.
-    private Transform CheckSafeJump(Vector2 target)
+    //Checks whether there is a lilypad at the given position and, if there is, returns it's transform.
+    private Transform CheckForLilypad(Vector2 target)
     {
         foreach (GameObject lilypad in GameObject.FindGameObjectsWithTag("Lilypad"))
         {
@@ -102,5 +135,16 @@ public class PlayerController : MonoBehaviour
             }
         }
         return null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fence"))
+        {
+            //Debug.Log("HIT FENCE");
+            targetJump = null;
+            isJumping = false;
+            gameManager.HitFence();
+        }
     }
 }
